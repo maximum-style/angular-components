b6e057c85a19dd47ce204c708e18b1136c133a82
fjtluz <fjtluz@proton.me>
2025-02-13 18:22:47 -0300

    Adding examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {showcaseRoutes} from './showcase/showcase.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'showcase',
+        children: showcaseRoutes
+    }
+];
