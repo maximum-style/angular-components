fe3a3e368924c045f4569621cea8d139afed32af
fjtluz <fjtluz@proton.me>
2025-02-17 18:19:24 -0300

    Adding ExampleComponent in showcase

--- src/app/showcase/example-showcase/example-showcase.component.ts
+++ src/app/showcase/example-showcase/example-showcase.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ExampleComponent } from '../../components/example/example.component';
 
 @Component({
-  imports: [],
+  imports: [ExampleComponent],
   templateUrl: './example-showcase.component.html',
   styleUrl: './example-showcase.component.scss'
 })
