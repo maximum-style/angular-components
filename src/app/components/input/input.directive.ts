4e5f36efa665e0574a50678e432d0dbccd9fdbe2
fjtluz <fjtluz@proton.me>
2025-03-04 11:44:45 -0300

    Adicionando eventos de valueChanges e valueUpdates ao input

--- src/app/components/input/input.directive.ts
+++ src/app/components/input/input.directive.ts
@@ -1,13 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, output } from '@angular/core';
 
 @Directive({
   selector: 'input[maxInput]',
   host: {
-      'class': 'max-input'
+      'class': 'max-input',
+      '(keyup)': 'onKeyUp($event.target)',
   }
 })
 export class InputDirective {
 
-  constructor() { }
+    private oldValue = '';
+    
+    public valueChanges = output<string>();
+    public valueUpdates = output<{ old: string; new: string; }>();
+
+    public onKeyUp(element: HTMLInputElement) {
+        const newValue = element.value;
+
+        this.valueChanges.emit(newValue);
+
+        if (this.oldValue !== newValue) {
+            this.valueUpdates.emit({ old: this.oldValue, new: newValue });
+            this.oldValue = newValue;
+        }
+    }
 
 }
--- src/app/showcase/input-showcase/input-showcase.component.ts
+++ src/app/showcase/input-showcase/input-showcase.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {InputDirective} from '@components/input/input.directive';
 import {ShowcaseExampleComponent} from '@showcase/showcase-example/showcase-example.component';
 
 @Component({
-  imports: [ShowcaseExampleComponent, InputDirective],
+  imports: [
+      ShowcaseExampleComponent, 
+      InputDirective,
+      FormsModule,
+      ReactiveFormsModule
+  ],
   templateUrl: './input-showcase.component.html',
   styleUrl: './input-showcase.component.scss'
 })
 export class InputShowcaseComponent {
+
+    public e3_value!: string;
+    public e4_formGroup: FormGroup;
+    public e5_value!: string;
+    public e6_value?: { old: string; new: string};
+
+    constructor() {
+        this.e4_formGroup = this.buildE4FormGroup();
+    }
+
+    private buildE4FormGroup() {
+        return new FormGroup({
+            e4_control: new FormControl<string>(''),
+        })
+    }
+
+    public E5_valueChanges(value: string) {
+        this.e5_value = value;
+    }
+
+    public E6_valueUpdates(value: { old: string; new: string; }) {
+        this.e6_value = value;
+    }
 }
+
