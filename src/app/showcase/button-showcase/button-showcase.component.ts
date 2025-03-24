1581665036530f218660007a4d75e7d74dae8705
Tiago Abila <tlabila@hotmail.com>
2025-03-24 14:05:30 -0300

    Adicionado font awesome

--- src/app/showcase/button-showcase/button-showcase.component.ts
+++ src/app/showcase/button-showcase/button-showcase.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { ButtonDirective } from '@components/button/button.directive';
 import { ShowcaseExampleComponent } from '@showcase/showcase-example/showcase-example.component';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   imports: [
     ButtonDirective,
-    ShowcaseExampleComponent
+    ShowcaseExampleComponent,
+    FontAwesomeModule
   ],
   templateUrl: './button-showcase.component.html',
   styleUrls: ['./button-showcase.component.scss']
 })
 export class ButtonShowcaseComponent {
+  faCoffee = faCoffee;
 
   public onClickEvent() {
     console.log('TO HABILITADO AAAAAAAAAA')
